8dee34f69317fa7637bc7404f7d1cbdd4433832f
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-25 16:03:53 +0300

    add: modal, pressable, statusBar

--- App.js
+++ App.js
@@ -5,12 +5,33 @@ import {
   Image,
   ImageBackground,
   Button,
+  Pressable,
+  Modal,
+  StatusBar,
 } from "react-native";
+import { useState } from "react";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isStatusbarHidden, setIsStatusbarHidden] = useState(false);
+
+  const modalVisibleHandler = (value) => {
+    return setIsModalVisible(value);
+  };
+
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <StatusBar
+        backgroundColor="lightgreen"
+        barStyle="dark-content"
+        hidden={isStatusbarHidden}
+      />
+      <Button
+        title="Hide Status Bar"
+        onPress={() => setIsStatusbarHidden((prevState) => !prevState)}
+        color="midnightblue"
+      />
       <ScrollView>
         {/* <ImageBackground source={logoImg} style={{ flex: 1 }}>
         <Text>
@@ -19,22 +40,26 @@ export default function App() {
         </Text>
       </ImageBackground> */}
 
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Pressable onPress={() => console.log("Image pressed")}>
+          <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        </Pressable>
+        <Pressable onPress={() => console.log("text pressed")}>
+          <Text>
+            Mollit aliqua id id anim adipisicing anim mollit enim adipisicing eu
+            et et. Ex non fugiat nostrud nisi est excepteur sunt commodo sint.
+            Duis pariatur ut aliquip quis magna sunt do ut. Mollit aliqua id id
+            anim adipisicing anim mollit enim adipisicing eu et et. Ex non
+            fugiat nostrud nisi est excepteur sunt commodo sint. Duis pariatur
+            ut aliquip quis magna sunt do ut.
+          </Text>
+        </Pressable>
         <Button
-          title="Press"
-          onPress={() => console.log("Button pressed")}
+          title="Press modal"
+          onPress={() => modalVisibleHandler(true)}
           color="midnightblue"
         />
-        <Text>
-          Mollit aliqua id id anim adipisicing anim mollit enim adipisicing eu
-          et et. Ex non fugiat nostrud nisi est excepteur sunt commodo sint.
-          Duis pariatur ut aliquip quis magna sunt do ut. Mollit aliqua id id
-          anim adipisicing anim mollit enim adipisicing eu et et. Ex non fugiat
-          nostrud nisi est excepteur sunt commodo sint. Duis pariatur ut aliquip
-          quis magna sunt do ut.
-        </Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
 
+        {/* <Image source={logoImg} style={{ width: 300, height: 300 }} /> */}
         {/* <Image
         source={{ uri: "https://picsum.photos/300" }}
         style={{ width: 300, height: 300 }}
@@ -44,6 +69,21 @@ export default function App() {
         World
       </Text> */}
       </ScrollView>
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => modalVisibleHandler(false)}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text>Modal content</Text>
+          <Button
+            title="Close"
+            color="midnightblue"
+            onPress={() => modalVisibleHandler(false)}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
