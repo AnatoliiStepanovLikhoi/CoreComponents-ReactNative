e624d2d6f7bb046c55be6b19def711031e4fb615
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-12 11:56:06 +0300

    add: styles inharitance

--- App.js
+++ App.js
@@ -3,6 +3,11 @@ import { View, Text, StyleSheet } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.darkMode}>
+        <Text style={styles.darkModeText}>
+          Style inharitance <Text style={styles.boldText}>in bold</Text>
+        </Text>
+      </View>
       <View
         style={[
           styles.box,
@@ -24,6 +29,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "plum", padding: 60 },
+  darkMode: {
+    backgroundColor: "black",
+  },
+  darkModeText: {
+    color: "white",
+  },
+  boldText: {
+    fontStyle: "bold",
+  },
   box: {
     width: 250,
     height: 250,
