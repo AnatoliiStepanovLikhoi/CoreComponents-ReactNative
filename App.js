4570735c29bc0628b3fba2847e8f80a03db49f19
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-14 11:49:52 +0300

    add: flexDirection property

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "blue", flex: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "green", flex: 3 }}>Box 2</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
       <Box style={{ backgroundColor: "black" }}>Box 3</Box>
       <Box style={{ backgroundColor: "pink" }}>Box 4</Box>
       <Box style={{ backgroundColor: "tomato" }}>Box 5</Box>
@@ -16,6 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
