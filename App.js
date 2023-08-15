003e6b77c436c1264129d30a83e2cbb404430c75
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-15 09:41:07 +0300

    add: alignSelf

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "blue", paddingVertical: 100 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "black" }}>Box 3</Box>
-      {/* <Box style={{ backgroundColor: "pink" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "tomato" }}>Box 5</Box> */}
+      <Box style={{ backgroundColor: "blue", alignSelf: "flex-start" }}>
+        Box 1
+      </Box>
+      <Box style={{ backgroundColor: "green", alignSelf: "flex-end" }}>
+        Box 2
+      </Box>
+      <Box style={{ backgroundColor: "black", alignSelf: "center" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "pink", alignSelf: "stretch" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "tomato", alignSelf: "auto" }}>Box 5</Box>
     </View>
   );
 }
@@ -16,8 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "baseline",
+    alignItems: "flex-end",
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
