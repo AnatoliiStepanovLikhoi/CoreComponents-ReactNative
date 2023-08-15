817112ad5cc20db7600a76c44f07feb9af82c9b6
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-15 09:45:04 +0300

    add: flexWrap

--- App.js
+++ App.js
@@ -4,15 +4,11 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "blue", alignSelf: "flex-start" }}>
-        Box 1
-      </Box>
-      <Box style={{ backgroundColor: "green", alignSelf: "flex-end" }}>
-        Box 2
-      </Box>
-      <Box style={{ backgroundColor: "black", alignSelf: "center" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "pink", alignSelf: "stretch" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "tomato", alignSelf: "auto" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "black" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "pink" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "tomato" }}>Box 5</Box>
     </View>
   );
 }
@@ -20,7 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "flex-end",
+    flexDirection: "row",
+    flexWrap: "wrap-reverse",
+    height: 300,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
