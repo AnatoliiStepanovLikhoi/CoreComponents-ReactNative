208979fed0a512aa930f781be6657a4106b4bbc9
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-17 22:12:13 +0300

    add: flex grow factor

--- App.js
+++ App.js
@@ -4,17 +4,13 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "blue", flexShrink: 1 }}>Box 1 shrink</Box>
-      <Box style={{ backgroundColor: "green", flexShrink: 2 }}>
-        Box 2 shrink
-      </Box>
-      {/* <Box style={{ backgroundColor: "black", flexBasis: 140, flex: 1 }}>
-        Box 3
-      </Box>
-      <Box style={{ backgroundColor: "pink", height: 140, flex: 1 }}>Box 4</Box>
-      <Box style={{ backgroundColor: "tomato" }}>Box 5</Box>
-      <Box style={{ backgroundColor: "plum" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "magenta" }}>Box 7</Box> */}
+      <Box style={{ backgroundColor: "blue" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "black" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "pink" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "tomato", flexGrow: 1 }}>Box 5</Box>
+      <Box style={{ backgroundColor: "plum", flexGrow: 3 }}>Box 6</Box>
+      <Box style={{ backgroundColor: "magenta" }}>Box 7</Box>
     </View>
   );
 }
@@ -22,9 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "flex-start",
-    width: 300,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
