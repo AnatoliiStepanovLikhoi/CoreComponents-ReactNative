4e679afd2ed59728957e7b7eebc723d786283e05
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-08-18 17:06:41 +0300

    add: absolute, relative layout

--- App.js
+++ App.js
@@ -4,12 +4,21 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "blue" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "blue", top: 75, left: 75 }}>Box 1</Box>
       <Box style={{ backgroundColor: "green" }}>Box 2</Box>
       <Box style={{ backgroundColor: "black" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "pink" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "tomato", flexGrow: 1 }}>Box 5</Box>
-      <Box style={{ backgroundColor: "plum", flexGrow: 3 }}>Box 6</Box>
+      <Box
+        style={{
+          backgroundColor: "pink",
+          position: "absolute",
+          top: 175,
+          left: 175,
+        }}
+      >
+        Box 4
+      </Box>
+      <Box style={{ backgroundColor: "tomato" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "plum" }}>Box 6</Box>
       <Box style={{ backgroundColor: "magenta" }}>Box 7</Box>
     </View>
   );
