531de9b2bc2f999ebcc0121b60dde9da1dc58c27
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-09-01 22:41:30 +0300

    add: orientation addaptive styles

--- App.js
+++ App.js
@@ -1,17 +1,45 @@
 import { View, StyleSheet, Text, Dimensions } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const subscriptions = Dimensions.addEventListener(
+      "change",
+      ({ window }) => {
+        setDimensions({ window });
+      }
+    );
+
+    return () => subscriptions?.remove();
+  });
+
+  const { window } = dimensions;
+
+  const windowWidth = window.width;
+  const windowHeigth = window.height;
+
+  // console.log(windowWidth, windowHeigth);
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeigth > 600 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? 50 : 24 }}>Welcome!</Text>
       </View>
     </View>
   );
 }
-
-const windowWidth = Dimensions.get("window").width;
-const windowHeigth = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -21,13 +49,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeigth > 600 ? "60%" : "90%",
+    // width: windowWidth > 500 ? "70%" : "90%",
+    // height: windowHeigth > 600 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
-  },
+  // text: {
+  //   fontSize: windowWidth > 500 ? 50 : 24,
+  // },
 });
